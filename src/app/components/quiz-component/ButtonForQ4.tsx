2db4fb8c5a27fdd5b0b8c64e4a9734a7c85fc33c
CoderAyushman <mishraayushman361@gmail.com>
2024-06-21 21:36:05 +0530

    adding functionality in QuizNo4.tsx

--- src/app/components/quiz-component/ButtonForQ4.tsx
+++ src/app/components/quiz-component/ButtonForQ4.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 interface MyComponentProps {
-  num: number;
+  num: boolean;
 }
 
 
@@ -10,7 +10,8 @@ const ButtunForQ4: React.FC<MyComponentProps> = ({num}) => {
   }
   return (
     <div className='text-center bg-white pb-[20px] pt-[20px] fixed w-full  bottom-0 '>
-      <button  onClick={handleClick}className='cursor-not-allowed  bg-customGreen pl-[130px] pr-[130px] pt-[15px] pb-[15px] rounded-full text-white  '><h1><b>NEXT STEP</b></h1></button>
+     {num?(<button  onClick={handleClick} className='cursor-pointer bg-customGreen pl-[130px] pr-[130px] pt-[15px] pb-[15px] rounded-full text-white  '><h1><b>NEXT STEP</b></h1></button>):(<button className='cursor-not-allowed  bg-gray-200 pl-[130px] pr-[130px] pt-[15px] pb-[15px] rounded-full text-gray-500  '><h1><b>NEXT STEP</b></h1></button>)}
+      
     </div>
   )
 }
--- src/app/components/quiz-component/QuizNo4.tsx
+++ src/app/components/quiz-component/QuizNo4.tsx
@@ -7,29 +7,36 @@ import { useAppDispatch } from "@/lib/hooks";
 import { increse } from "@/lib/features/counter/counterSlice";
 import radioValues from "../RadioValues";
 import ButtonForQ4 from "./ButtonForQ4";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 const QuizNo3 = () => {
-  const [counter,setCounter]=useState<number>(0);
+  // const [counter, setCounter] = useState<number>(0);
+  const [isCheckedForQ1, setIsCheckedForQ1] = useState(false);
+  const [isCheckedForQ2, setIsCheckedForQ2] = useState(false);
+  const [isCheckedForQ3, setIsCheckedForQ3] = useState(false);
+  const [isCheckedForQ4, setIsCheckedForQ4] = useState(false);
+  const [isCheckedForQ5, setIsCheckedForQ5] = useState(false);
+  let count=isCheckedForQ1 || isCheckedForQ2 || isCheckedForQ3 || isCheckedForQ4 || isCheckedForQ5;
   const dispatch = useAppDispatch();
   const handleClickOnArms = () => {
-    setCounter(1);
+    setIsCheckedForQ1(!isCheckedForQ1);
     radioValues[2] = "slender";
   };
   const handleClickOnPecs = () => {
+    setIsCheckedForQ2(!isCheckedForQ2);
     radioValues[2] = "athletic";
-    setCounter(1);
   };
   const handleClickOnBelly = () => {
+    setIsCheckedForQ3(!isCheckedForQ3);
     radioValues[2] = "shredded";
-    setCounter(1);
   };
   const handleClickOnLegs = () => {
+    setIsCheckedForQ4(!isCheckedForQ4);
     radioValues[2] = "swole";
-    setCounter(1);
   };
   const handleClickOnBack = () => {
+    setIsCheckedForQ5(!isCheckedForQ5);
     radioValues[2] = "swole";
-    setCounter(1);
   };
   return (
     <div className="flex-col justify-center items-center">
@@ -42,8 +49,6 @@ const QuizNo3 = () => {
       <div className="flex justify-evenly items-center text-center mt-5">
         <div>
           <Label
-            onSelect={handleClickOnArms}
-            
             htmlFor="r1"
             className="flex justify-between shadow-md rounded-xl border border-gray-200 items-center w-[460px]  transform duration-500 hover:scale-[96%] cursor-pointer mt-5"
           >
@@ -53,11 +58,16 @@ const QuizNo3 = () => {
               alt="lose-weight-img"
             />
             <h1 className="pr-[175px] font-semibold text-base ">Arms</h1>
-            <Checkbox className="mr-5 " id="r1" />
+            <Checkbox
+              className="mr-5 "
+              id="r1"
+              value="arms"
+              checked={isCheckedForQ1}
+              onCheckedChange={handleClickOnArms}
+            />
           </Label>
 
           <Label
-            onClick={handleClickOnPecs}
             htmlFor="r2"
             className="flex justify-between shadow-xl rounded-xl border  border-gray-200 items-center w-[460px] transform duration-500 hover:scale-[96%] cursor-pointer mt-5"
           >
@@ -68,10 +78,15 @@ const QuizNo3 = () => {
             />
 
             <h1 className="pr-[175px] font-semibold text-base">Pecs</h1>
-            <Checkbox className="mr-5" value="athletic" id="r2" />
+            <Checkbox
+              className="mr-5"
+              value="pecs"
+              id="r2"
+              onCheckedChange={handleClickOnPecs}
+            />
           </Label>
+
           <Label
-            onClick={handleClickOnBelly}
             htmlFor="r3"
             className="flex justify-between shadow-xl rounded-xl border  border-gray-200 items-center w-[460px] transform duration-500 hover:scale-[96%] cursor-pointer mt-5"
           >
@@ -82,10 +97,15 @@ const QuizNo3 = () => {
             />
 
             <h1 className="pr-[175px] font-semibold text-base">Belly</h1>
-            <Checkbox className="mr-5" value="shredded" id="r3" />
+            <Checkbox
+              className="mr-5"
+              value="belly"
+              id="r3"
+              onCheckedChange={handleClickOnBelly}
+            />
           </Label>
+
           <Label
-            onClick={handleClickOnLegs}
             htmlFor="r4"
             className="flex justify-between shadow-xl rounded-xl border  border-gray-200 items-center w-[460px] transform duration-500 hover:scale-[96%] cursor-pointer mt-5"
           >
@@ -97,10 +117,15 @@ const QuizNo3 = () => {
             />
 
             <h1 className="pr-[175px] font-semibold text-base">Legs</h1>
-            <Checkbox className="mr-5" value="swole" id="r4" />
+            <Checkbox
+              className="mr-5"
+              value="legs"
+              id="r4"
+              onCheckedChange={handleClickOnLegs}
+            />
           </Label>
+
           <Label
-            onClick={handleClickOnBack}
             htmlFor="r5"
             className="flex justify-between shadow-xl rounded-xl border  border-gray-200 items-center w-[460px] transform duration-500 hover:scale-[96%] cursor-pointer mt-5"
           >
@@ -112,14 +137,19 @@ const QuizNo3 = () => {
             />
 
             <h1 className="pr-[175px] font-semibold text-base">Back</h1>
-            <Checkbox className="mr-5" value="swole" id="r5" />
+            <Checkbox
+              className="mr-5"
+              value="back"
+              id="r5"
+              onCheckedChange={handleClickOnBack}
+            />
           </Label>
         </div>
       </div>
       <Footer />
       <div className="p-[50px]"></div>
 
-      <ButtonForQ4 num={counter} />
+      <ButtonForQ4 num={count} />
     </div>
   );
 };
