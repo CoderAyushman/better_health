f4309acf5e5b98735ef491e0f91b93c1834818a0
CoderAyushman <mishraayushman361@gmail.com>
2024-09-14 23:32:22 +0530

    changing markdown value

--- src/app/components/Answer.tsx
+++ src/app/components/Answer.tsx
@@ -14,28 +14,32 @@ import "@/app/components/tableSheet.css";
 import ResetAllDataBtn from "./ResetAllDataBtn";
 
 const Output = () => {
-  const [markdownText, setMarkdownText] = useState < any > ("null");
+  const [markdownText, setMarkdownText] = useState < any > (radioValues[35].response);
   const MODEL_NAME = "gemini-1.0-pro";
   const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY as string;
 
-  console.log(API_KEY, MODEL_NAME)
+  // console.log(API_KEY, MODEL_NAME)
   //query from gemini
-  const runChat = async () => {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  useEffect(() => {
+    const runChat = async () => {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-    const result = await model.generateContent(radioValues[34].message);
-    const response = result.response.text();
-    console.log(typeof radioValues[34].message);
-    console.log(response);
-    setMarkdownText(response);
-  }
-  useEffect(() => {
-    runChat();
+      const result = await model.generateContent(radioValues[34].message);
+      const response = result.response.text();
+      // console.log(typeof radioValues[34].message);
+      // console.log(response);
+      setMarkdownText(response);
+      radioValues[35].response = response;
+    }
+    if (markdownText === null) {
+
+      runChat();
+    }
+    else {
+      setMarkdownText(radioValues[35].response);
+    }
   }, []);
-  useEffect(() => {
-    console.log(typeof markdownText);
-  }, [markdownText]);
   return (
     <main className="flex items-center justify-center mt-32 ">
       <div className="flex-col items-center justify-center ml-10 mr-10">
@@ -47,7 +51,7 @@ const Output = () => {
         <div className="z-10 relative">
           <h1>Here yours ai generated full personal Diet.</h1>
 
-          {markdownText == "null" ? <Skeleton count={50} height={50} /> : <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {markdownText === null ? <Skeleton count={50} height={50} /> : <ReactMarkdown remarkPlugins={[remarkGfm]}>
             {markdownText}
           </ReactMarkdown>}
 
