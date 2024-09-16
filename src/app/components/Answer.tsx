588a379d2d6e893ffc68a5761dc1cff8ea876ecd
CoderAyushman <mishraayushman361@gmail.com>
2024-09-16 23:34:34 +0530

    adding download btn

--- src/app/components/Answer.tsx
+++ src/app/components/Answer.tsx
@@ -6,23 +6,44 @@ import {
 } from "@google/generative-ai";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { radioValues } from "../components/RadioValues";
 import "@/app/components/tableSheet.css";
 import ResetAllDataBtn from "./ResetAllDataBtn";
+import generatePDF, { Resolution, Margin } from "react-to-pdf";
 
 const Output = () => {
+  const options: any = {
+    resolution: Resolution.HIGH,
+    filename: "better-health-diet-plan.pdf",
+    page: {
+      margin: Margin.SMALL,
+      format: "letter",
+    },
+    canvas: {
+      qualityRatio: 1,
+    },
+    overrides: {
+      pdf: {
+        compress: true,
+      },
+      canvas: {
+        useCORS: true,
+      },
+    },
+  };
+
+  const getTargetElement = useRef<any>();
   const [markdownText, setMarkdownText] = useState<any>(null);
   const MODEL_NAME = "gemini-1.0-pro";
   const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY as string;
 
-  // console.log(API_KEY, MODEL_NAME)
   //query from gemini
   useEffect(() => {
     const localStoreResponse = localStorage.getItem("ai-response");
-    // console.log("localstore response", localStoreResponse);
+
     const runChat = async () => {
       const genAI = new GoogleGenerativeAI(API_KEY);
       const model = genAI.getGenerativeModel({ model: MODEL_NAME });
@@ -30,8 +51,7 @@ const Output = () => {
       const result = await model.generateContent(radioValues[34].message);
       const response = result.response.text();
       localStorage.setItem("ai-response", response);
-      // console.log(typeof radioValues[34].message);
-      // console.log(response);
+
       setMarkdownText(response);
     };
     if (!localStoreResponse) {
@@ -50,17 +70,36 @@ const Output = () => {
           className="h-[600px] ml-[50vw]  fixed z-0 opacity-30 md:w-[700px] md:ml-[40vw]  "
         />
         <div className="z-10 relative">
-          <h1>Here yours ai generated full personal Diet.</h1>
+          <div ref={getTargetElement} className="">
+            <h1>Here yours ai generated full personal Diet.</h1>
 
-          {markdownText === null ? (
-            <Skeleton count={50} height={50} />
-          ) : (
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-              {markdownText}
-            </ReactMarkdown>
-          )}
-
-          <ResetAllDataBtn />
+            {markdownText === null ? (
+              <Skeleton count={50} height={50} />
+            ) : (
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {markdownText}
+              </ReactMarkdown>
+            )}
+            <div className="flex justify-between items-center">
+              <h1 className="text-md md:text-xl  font-sans text-[#D2B37C] font-bold">
+                {new Date().toUTCString()}
+              </h1>
+              <img
+                src="better-health-seal.png"
+                alt="better-heath-seal"
+                className="w-[120px] md:w-[200px]"
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-start gap-2">
+            <button
+              onClick={() => generatePDF(getTargetElement, options)}
+              className="w-[30px] md:w-[50px] bg-customGreen rounded-md overflow-visible  text-white py-2 mb-5   px-2"
+            >
+              <img src="download.png" />
+            </button>
+            <ResetAllDataBtn />
+          </div>
         </div>
       </div>
     </main>
--- src/app/components/ResetAllDataBtn.tsx
+++ src/app/components/ResetAllDataBtn.tsx
@@ -1,68 +1,59 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { useRouter } from 'next/navigation'
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
 const ResetAllDataBtn = () => {
-    const router = useRouter();
-    const handleOnclick = () => {
-        // localStorage.removeItem('counter');
-        // localStorage.removeItem('data');
-        localStorage.clear();
+  const router = useRouter();
+  const handleOnclick = () => {
+    // localStorage.removeItem('counter');
+    // localStorage.removeItem('data');
+    localStorage.clear();
 
+    window.location.assign("/");
+  };
+  return (
+    <div className="flex justify-center items-center">
+      <Dialog>
+        <DialogTrigger>
+          <button className="flex w-[30px] md:w-[50px] text-sm  md:text-base bg-customGreen rounded-md overflow-visible  text-white p-2 mb-5  ">
+            <img src="reset.png" alt="reset" />
+            {/* <img src="reset-buttons.png" alt="reset button" className='ml-3 max-w-[30px]' /> */}
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This will permanently delete your given data and ai generated diet
+              plans.
+            </DialogDescription>
+            <button
+              onClick={handleOnclick}
+              className=" bg-customGreen rounded-full overflow-visible  text-white py-2 mb-5   px-2 "
+            >
+              <b>Reset</b>
+            </button>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
 
-        window.location.assign('/');
+    // <button
+    //     onClick={handleOnclick}
 
+    //     className="bg-customGreen rounded-full overflow-visible  text-white pt-5 mt-[10px] pb-5  pl-28 pr-28 "
+    // >
+    //     <b>CONTINUE</b>
+    // </button>
+  );
+};
 
-    }
-    return (
-        <div className="flex justify-center items-center">
-
-            <Dialog>
-                <DialogTrigger >
-                    <button
-
-
-                        className="flex text-sm  md:text-base bg-customGreen rounded-full overflow-visible  text-white py-5 mt-[10px]   px-28 my-5"
-                    >
-                        <b>Reset All Data</b>
-                        {/* <img src="reset-buttons.png" alt="reset button" className='ml-3 max-w-[30px]' /> */}
-                    </button></DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Are you absolutely sure?</DialogTitle>
-                        <DialogDescription>
-                            This action cannot be undone. This will permanently delete your given data and ai generated diet plans.
-                        </DialogDescription>
-                        <button
-
-                            onClick={handleOnclick}
-                            className=" bg-customGreen rounded-full overflow-visible  text-white py-2 my-5   px-2 "
-                        >
-                            <b>Reset</b>
-
-                        </button>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
-        </div>
-
-        // <button
-        //     onClick={handleOnclick}
-
-        //     className="bg-customGreen rounded-full overflow-visible  text-white pt-5 mt-[10px] pb-5  pl-28 pr-28 "
-        // >
-        //     <b>CONTINUE</b>
-        // </button>
-
-    )
-}
-
-export default ResetAllDataBtn
+export default ResetAllDataBtn;
