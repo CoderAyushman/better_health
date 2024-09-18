edd6c6c1da66bcbd0cbb8c908006e5e7bd501775
CoderAyushman <mishraayushman361@gmail.com>
2024-09-18 23:38:27 +0530

    changing height of introfemale page

--- src/app/introfemale/page.tsx
+++ src/app/introfemale/page.tsx
@@ -9,10 +9,9 @@ const Introfemale = () => {
   const dispatch = useAppDispatch();
   const handleOnclick = () => {
     dispatch(increse());
-
   };
   return (
-    <div className="mt-32 flex-col justify-center text-center items-center">
+    <div className="mt-32 flex-col min-h-[100vh] w-[100vw]  justify-center text-center items-center">
       <p className="pb-10">
         <span className="text-customGreen font-bold text-5xl">
           150 million people
@@ -25,16 +24,12 @@ const Introfemale = () => {
       </div>
       <button
         onClick={handleOnclick}
-
-        className="bg-customGreen rounded-full overflow-visible  text-white pt-5  pb-5  pl-28 pr-28 "
+        className="bg-customGreen rounded-full overflow-visible  text-white pt-5 md:mt-[-10px] pb-5  pl-28 pr-28  "
       >
         <b>CONTINUE</b>
       </button>
 
-      <div className="bottom-0">
-
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
