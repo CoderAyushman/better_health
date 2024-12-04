220fea14b5e9e34e6fa58f76198da526c89d90cd
CoderAyushman <mishraayushman361@gmail.com>
2024-12-04 11:44:31 +0530

    solve errors occurs during deployment

--- src/utils/getAnswer.tsx
+++ src/utils/getAnswer.tsx
@@ -1,19 +1,19 @@
-"use server";
-import { radioValues } from "@/app/components/RadioValues";
-import axios from "axios";
+// "use server";
+// import { radioValues } from "@/app/components/RadioValues";
+// import axios from "axios";
 
-export const getAnswer = async (msg: any) => {
-  axios
-    .post("http://localhost:8000/api/langchain/gemini", {
-      prompt: msg,
-    })
-    .then(async (response) => {
-      console.log(await response.data.response);
-      radioValues[35].response = await response.data.response;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+// export const getAnswer = async (msg: any) => {
+//   axios
+//     .post("http://localhost:8000/api/langchain/gemini", {
+//       prompt: msg,
+//     })
+//     .then(async (response) => {
+//       console.log(await response.data.response);
+//       radioValues[35].response = await response.data.response;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
 
-// export default getAnswer;
+// // export default getAnswer;
